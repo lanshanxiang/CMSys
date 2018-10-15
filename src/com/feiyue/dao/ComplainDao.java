49c7a455ecbbcec8355002cff2ea7d2285dab2ce
Administrator <Administrator@PC201804121440>
2018-10-15 14:31:58 +0800

    增加了投诉功能

---
+++ src/com/feiyue/dao/ComplainDao.java
@@ -0,0 +1,21 @@
+package com.feiyue.dao;
+
+import java.util.List;
+
+
+import com.feiyue.entiy.ComplainBean;
+/**
+ * 处理数据库交互的DAO
+ * @author Administrator
+ *
+ */
+public interface ComplainDao {
+	 //查询所有方法
+     public List<ComplainBean> queryComplain();
+     //增加操作
+     public boolean addComplain(ComplainBean c);
+     //修改操作
+     public boolean updateComplain(ComplainBean c);
+     //删除操作
+     public boolean deleteComplain(int conplyId);
+}
---
+++ src/com/feiyue/dao/impl/ComplainDaoImpl.java
@@ -0,0 +1,39 @@
+package com.feiyue.dao.impl;
+
+import java.util.List;
+
+import com.feiyue.dao.ComplainDao;
+import com.feiyue.entiy.ComplainBean;
+import com.feiyue.util.DBUtil;
+/**
+ * 与数据库交互的DAO实现类
+ * @author Administrator
+ *
+ */
+public class ComplainDaoImpl implements ComplainDao {
+	//查询所有
+	@Override
+	public List<ComplainBean> queryComplain() {
+		// TODO Auto-generated method stub
+		return (List<ComplainBean>) DBUtil.select("select * from tb_complain", ComplainBean.class);
+	}
+    //增加
+	@Override
+	public boolean addComplain(ComplainBean c) {
+		// TODO Auto-generated method stub
+		return DBUtil.execute("INSERT INTO tb_complain VALUES (null, ?, ?, ?, ?, ?, ?, ?)", c.getConplyName(),c.getConplyQuestion(),c.getConplyTime(),c.getConplyTel(),c.getConplyPhone(),c.getConplyStatus(),c.getConplyRemark())>0;
+	}
+    //修改
+	@Override
+	public boolean updateComplain(ComplainBean c) {
+		// TODO Auto-generated method stub
+		return DBUtil.execute("UPDATE tb_complain SET `conplyName`=?, `conplyQuestion`=?, `conplyTime`=?, `conplyTel`=?, `conplyPhone`=?, `conplyStatus`=?, `conplyRemark`=? WHERE (`conplyId`=?);", c.getConplyName(),c.getConplyQuestion(),c.getConplyTime(),c.getConplyTel(),c.getConplyPhone(),c.getConplyStatus(),c.getConplyRemark(),c.getConplyId())>0;
+	}
+    //删除
+	@Override
+	public boolean deleteComplain(int conplyId) {
+		// TODO Auto-generated method stub
+		return DBUtil.execute("delete from tb_complain where conplyId=?", conplyId)>0;
+	}
+
+}
